a23f636d4dee8a3ffcfe885ae89c370524c33560
itshoang2024 <158324689+itshoang2024@users.noreply.github.com>
2025-05-18 16:12:02 +0700

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -29,13 +29,13 @@ type SocialLink = {
 
 export const EMAIL = ''
 
-export const WEB_TITLE = 'Name - Personal Website'
+export const WEB_TITLE = 'Trần Vinh - Personal Website'
 export const WEB_DESCRIPTION =
   'A personal website showcasing my work and projects.'
-export const FOOTER_COPYRIGHT = '© 2025 Nguyen Van B.'
+export const FOOTER_COPYRIGHT = '© 2025 Trần Vinh.'
 export const FOOTER_LINK = 'https://github.com/username/'
 
-export const NAME = 'Tran Vinh'
+export const NAME = 'Trần Vinh'
 export const JOB_TITLE = 'IT Student'
 export const DESCRIPTION =
   'I am a passionate designer and developer with a focus on creating beautiful and functional user interfaces. I have experience in design engineering, front-end development, and product design. I love to explore the intersection of design and technology, and I am always looking for new challenges to tackle.'
